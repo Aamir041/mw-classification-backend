86682b6667ee4f63b96779ab154a092b8b91effa
Aamir041 <aamirsaudagar34@gmail.com>
2024-05-02 23:06:44 +0530

    added helper func to get hospital id from token

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,19 +1,27 @@
+const {SECRET_KEY} = require("../constants/constants")
 exports = {}
 
 const jwt = require("jsonwebtoken");
 
 exports.getTokens = async(user) => {
-    const tokenUserDetails = {
+    const userDetailsAsIdentifier = {
         id: user.id,
         username : user.username,
         hospital_id: user.hospital_id
     }
     const token = jwt.sign(
-        { identifier: tokenUserDetails },
-        "mwapp"
+        { identifier: userDetailsAsIdentifier},
+        SECRET_KEY
     );
 
     return token;
+}
+
+exports.getHospitalIdFromToken = async (bearerToken) => {
+    const token = bearerToken.substring(7); // get token from bearerToken "Bearer abcd..."
+    const decoded = jwt.verify(token, SECRET_KEY); // decode token where we get {identifier and iat}
+    const hospital_id = decoded.identifier.hospital_id; // get identifier.hospital_id
+    return hospital_id;
 }
 
 module.exports = exports;
